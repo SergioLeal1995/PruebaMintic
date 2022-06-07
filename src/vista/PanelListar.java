f9762d13a22cf6bdcf6aede1aa010a5c9c93b153
Danilo97ram <danilo.ramirez.gomez@outlook.es>
2022-06-07 12:59:47 -0500

    Campo de listar listo, creacion de la tabla.

--- src/vista/PanelListar.java
+++ src/vista/PanelListar.java
@@ -4,59 +4,65 @@ import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
 
 public class PanelListar extends JPanel {
     private JLabel e_ListarProducto;
     private JLabel e_Vacio;
-    private JButton b_Ver_PListar;
     private JButton b_Volver_PListar;
+    private JTable tablaDatos;
     
     public PanelListar() {
-        setLayout(new GridLayout(5, 3, 40, 20));
+        setLayout(null);
         setBackground(new Color(10,148,100));
         
         inicializarComponentes();
-
         setVisible(true);
     }
+    
     public void inicializarComponentes(){
-        e_Vacio =  new JLabel("");
-        add(e_Vacio);
         e_Vacio =  new JLabel("Listar Productos", SwingConstants.CENTER);
-        add(e_Vacio);
-        e_Vacio =  new JLabel("");
+        e_Vacio.setBounds(150,10,150,30);
         add(e_Vacio);
         //2
-        e_Vacio =  new JLabel("ACA",SwingConstants.CENTER);
-        add(e_Vacio);
-        e_Vacio =  new JLabel("VA",SwingConstants.CENTER);
-        add(e_Vacio);
-        e_Vacio =  new JLabel("LA",SwingConstants.CENTER);
-        add(e_Vacio);
-        //3
-        e_Vacio =  new JLabel("BASE",SwingConstants.CENTER);
-        add(e_Vacio);
-        e_Vacio =  new JLabel("DE",SwingConstants.CENTER);
-        add(e_Vacio);
-        e_Vacio =  new JLabel("DATOS",SwingConstants.CENTER);
-        add(e_Vacio);
-        //4
-        e_Vacio =  new JLabel("");
-        add(e_Vacio);
-        e_Vacio =  new JLabel("");
-        add(e_Vacio);
-        e_Vacio =  new JLabel("");
-        add(e_Vacio);
+
+        tablaDatos = new JTable();      
+        DefaultTableModel mod=  new DefaultTableModel();
+
+        mod.addColumn("Codigo del Producto");
+        mod.addColumn("Nombre del Producto");
+        mod.addColumn("Valor de Compra");
+        mod.addColumn("Valor de Venta");
+        mod.addColumn("Cantidad");
+        mod.addColumn("Categoria");
+
+        tablaDatos.setModel(mod);
+
+        String [] resultados = new String[6]; //String con datos
+        
+                resultados[0]= "123";
+                resultados[1]= "audio";
+                resultados[2]= "123";
+                resultados[3]= "233";
+                resultados[4]= "1323";
+                resultados[5]= "es";
+                mod.addRow(resultados);
+                
+        JScrollPane scroll= new JScrollPane(tablaDatos);
+        scroll.setBounds(10,100,500,150);
+        add(scroll);
+
+        scroll.setVisible(true);        
+        
         //5
-        e_Vacio =  new JLabel("");
-        add(e_Vacio);
+        
         b_Volver_PListar = new JButton("Volver");
         b_Volver_PListar.setActionCommand("volver_PanelListar");//label para accion
+        b_Volver_PListar.setBounds(150, 300, 150, 30);
         add(b_Volver_PListar);
-        e_Vacio =  new JLabel("");
-        add(e_Vacio);
-        
     }
 
     public JLabel getE_ListarProducto() {
